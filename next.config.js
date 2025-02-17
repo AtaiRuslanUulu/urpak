6bb327442aed8726873831bcd5f285258299157d
AtaiRuslanUulu <95936930+AtaiRuslanUulu@users.noreply.github.com>
2025-02-17 18:00:58 +0100

    moved files

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["127.0.0.1", "localhost"], // Добавляем локальный сервер
-  },
-};
-
-module.exports = nextConfig;
