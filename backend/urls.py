bb97aba14f9df85fa466887e2ae5823c6e1e0f5a
AtaiRuslanUulu <95936930+AtaiRuslanUulu@users.noreply.github.com>
2025-05-04 18:29:47 +0200

    added backend admin

--- backend/urls.py
+++ backend/urls.py
@@ -1,17 +1,9 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from django.http import HttpResponse
-from backend.views import DeveloperViewSet, ProjectViewSet, ApartmentViewSet, home
+from backend.views import DeveloperViewSet, ProjectViewSet, ApartmentViewSet
 
 router = DefaultRouter()
-router.register(r"developers", DeveloperViewSet)
-router.register(r"projects", ProjectViewSet)
-router.register(r"apartments", ApartmentViewSet)
+router.register(r"developers", DeveloperViewSet, basename="developer")
+router.register(r"projects", ProjectViewSet, basename="project")
+router.register(r"apartments", ApartmentViewSet, basename="apartment")
 
-def home(request):
-    return HttpResponse("<h1>Welcome to Real Estate API</h1>")
-
-urlpatterns = [
-    path("", home),
-    path("api/", include(router.urls)),
-]
+urlpatterns = router.urls
