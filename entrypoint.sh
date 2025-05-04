e53ee653e272a5af984ab101fd484cca249f8776
AtaiRuslanUulu <95936930+AtaiRuslanUulu@users.noreply.github.com>
2025-05-04 22:50:38 +0200

    auto‑create superuser in prod

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,13 @@ python manage.py migrate --noinput
 # (Optional) Collect static files
 python manage.py collectstatic --noinput
 
-python manage.py createsuperuser --no-input || true
+if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$RUN_SUPERUSER" != "no" ]; then
+  echo "Creating superuser $DJANGO_SUPERUSER_USERNAME ..."
+  python manage.py createsuperuser \
+    --no-input \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL" || true
+fi
 
 # Start Gunicorn
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
