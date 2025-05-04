566feac57b4b685d548fe287c6d3fb849770c0f1
AtaiRuslanUulu <95936930+AtaiRuslanUulu@users.noreply.github.com>
2025-05-04 21:31:16 +0200

    auto createsuperuser in production

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,7 @@ python manage.py migrate --noinput
 # (Optional) Collect static files
 python manage.py collectstatic --noinput
 
+python manage.py createsuperuser --no-input || true
+
 # Start Gunicorn
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
