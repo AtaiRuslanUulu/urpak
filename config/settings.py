60f7cc530bb8a8da2b6b2ead4aa4f19abdcce528
AtaiRuslanUulu <95936930+AtaiRuslanUulu@users.noreply.github.com>
2025-05-06 18:32:43 +0200

    fix: images from s3

--- config/settings.py
+++ config/settings.py
@@ -113,9 +113,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
 # CORS and CSRF helpers
 def _env_list(key):
     return [v.strip() for v in os.getenv(key, '').split(',') if v.strip()]
